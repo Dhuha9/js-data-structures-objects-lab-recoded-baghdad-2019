7b236401f75cca7b0b59b4e1acebac1d96272d2b
Dhuha Hasan <duha_9@yahoo.com>
2019-10-20 19:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function updateDriverWithKeyAndValue(driver,key,value){
   return Object.assign({},driver,{key:value});
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver,key,valu){
-  return Object.assign(driver,{[key]:value});
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+  driver[key] = value;
+ 
+  return driver;
+ 
 }
