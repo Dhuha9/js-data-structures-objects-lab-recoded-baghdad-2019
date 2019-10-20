8a76a05401797c2d37fc31445d54c1b9c74d07a6
Dhuha Hasan <duha_9@yahoo.com>
2019-10-20 19:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
 let driver={name:'Dhuha',filedStudy:'computer science'};
 function updateDriverWithKeyAndValue(driver,key,value){
-  return Object.assign
+  return Object.assign({},driver,{key:value});
 }
